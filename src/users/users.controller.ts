61e2f7295078bb73f8fb47b3ed68d3bd942e5c64
radenlarko <radenlarko@gmail.com>
2023-04-27 16:35:56 +0700

    Setup Prisma Setup User model

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.prismaService.user.findMany();
+  }
+}
